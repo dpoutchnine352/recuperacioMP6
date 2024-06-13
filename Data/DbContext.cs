73c07ff6ec933f62d615c1c7ba7683def40f1ef9
denis <denis@sge.cat>
2024-06-13 21:39:11 +0200

    DbContext creat

---
+++ Data/DbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace dpc.InternetSalesAPI
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<E-Commerce> E-Commerces { get; set; }
+        public DbSet<Shipping> Shippings { get; set; }
+        public DbSet<CreditCard> CreditCards { get; set; }
+        
+    }
+}
